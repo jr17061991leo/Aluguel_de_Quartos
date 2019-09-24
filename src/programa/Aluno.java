02335eda176fc3a32231df0c34390b98c17070df
jr17061991leo <jr17061991leo@gmail.com>
2019-09-23 21:33:53 -0300

    aluguel de quartos

---
+++ src/programa/Aluno.java
@@ -0,0 +1,31 @@
+package programa;
+
+public class Aluno {
+
+	private String nome, email;
+
+	public Aluno(String nome, String email) {
+
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
---
+++ src/programa/pensionato.java
@@ -0,0 +1,19 @@
+package programa;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class pensionato {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner in = new Scanner(System.in);
+
+		int[] quarto = new int[10];
+
+		System.out.println("Número de estudantes: ");
+		int numEstudantes = in.nextInt();
+
+	}
+
+}
