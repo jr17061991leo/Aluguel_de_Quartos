b2c6f31f845c00ae0611a034bd9fb1d0efe24135
jr17061991leo <jr17061991leo@gmail.com>
2019-09-23 23:08:17 -0300

    programa de aluguel de quartos, le os alunos a serem lotados em quartos gerando relatorio de quartos

--- src/programa/pensionato.java
+++ src/programa/pensionato.java
@@ -12,7 +12,7 @@ public class pensionato {
 		Scanner in = new Scanner(System.in);
 
 		int[] quarto = new int[10];
-		Aluno dadosAluno = new Aluno();
+		Aluno[] dadosAluno = new Aluno[10];
 
 		System.out.println("Número de estudantes: ");
 		int numEstudantes = in.nextInt();
@@ -20,12 +20,19 @@ public class pensionato {
 
 		for (int i = 1; i <= numEstudantes; i++) {
 			System.out.println("escolha o quarto do estudante " + i + ":");
-			int numQuarto = in.nextInt();
-			quarto[numQuarto] = in.nextInt();
+			quarto[i] = in.nextInt();
+			in.nextLine();
 			System.out.println("digite o nome do aluno e o email: ");
 			String nome = in.nextLine();
 			String Email = in.nextLine();
-			dadosAluno = new Aluno(nome, Email);
+			dadosAluno[i] = new Aluno(nome, Email);
+
+		}
+		for (int i = 0; i < quarto.length; i++) {
+			if (quarto[i] != 0) {
+				System.out.println(quarto[i] + ": " + dadosAluno[i].getNome() + ", " + dadosAluno[i].getEmail());
+
+			} else {};
 
 		}
 
